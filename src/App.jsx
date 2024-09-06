856d91c6f3adeae26ce1464d21353d799ca190da
Rodrigo Gamboa <rodrigogamboatorres@gmail.com>
2024-09-06 15:03:03 -0600

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import protobuf from "protobufjs";
 import { Buffer } from "buffer";
 import "./App.css";
 
+function formatPrice(price) {
+  return price ? `$${price.toFixed(2)}` : "";
+}
+
 function App() {
+  const [current, setCurrent] = useState(null);
+  const [stockUp, setStockUp] = useState(true);
+
   useEffect(() => {
     const ws = new WebSocket("wss://streamer.finance.yahoo.com");
     const root = protobuf.load("./YPricingData.proto", (error, root) => {
@@ -26,7 +33,10 @@ function App() {
       };
 
       ws.onmessage = function incoming(message) {
-        console.log(Yaticker.decode(Buffer(message.data, "base64")));
+        const next = Yaticker.decode(Buffer(message.data, "base64"));
+        // console.log(next?.price, current?.price);
+        setStockUp(next?.price > current?.price);
+        setCurrent(next);
       };
     });
   }, []);
@@ -34,6 +44,16 @@ function App() {
   return (
     <div>
       <h1>Yahoo Finance</h1>
+      <p>
+        Price: {formatPrice(current?.price)}
+        <span
+          style={{
+            color: stockUp ? "green" : "red",
+          }}
+        >
+          {stockUp ? "↑" : "↓"}
+        </span>
+      </p>
     </div>
   );
 }
